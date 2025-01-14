acf0b627d157f594adfc5ec309679f21c1094d3e
RoccoIntini <rocco.intini@studenti.unitn.it>
2025-01-14 13:19:36 +0100

    Update page.jsx

--- smarttn/app/viewCourseUser/page.jsx
+++ smarttn/app/viewCourseUser/page.jsx
@@ -1,4 +1,4 @@
-import Courses_list_User from '@/components/Courses_list_user';
+import Courses_list_User from '@/components/Course_list_user';
 
 export default function CoursesListPage() {
   return <Courses_list_User />;
