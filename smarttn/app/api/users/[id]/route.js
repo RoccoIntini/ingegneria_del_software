ab30cd252558b8afe26692f5ce6efcf116fb16f5
RoccoIntini <rocco.intini@studenti.unitn.it>
2025-01-14 12:54:24 +0100

    Update route.js

--- smarttn/app/api/users/[id]/route.js
+++ smarttn/app/api/users/[id]/route.js
@@ -1,18 +1,31 @@
 import User from "@/libs/models/user";
 import connectMongoDB from "@/libs/mongodb";
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
 
-export async function PATCH(request, {params}){
-    const {id} = params;
-    const  {newUsername: username, newPassword: password, newRole: role} = await request.json();
-    await connectMongoDB();
-    await User.findByIdAndUpdate(id, {username, password, role});
-    return NextResponse.json({message: "Utente modificato"}, {status: 200});
-}
+export async function PATCH(request, { params }) {
+    const { id } = params;
+    const { newUsername: username, newPassword: password, newRole: role } = await request.json();
 
-export async function GET(request, {params}){
-    const {id} = params;
     await connectMongoDB();
-    const user = await User.findOne({_id: id});
-    return NextResponse.json(user, {status: 200});
+
+    // Prepara l'oggetto di aggiornamento
+    const updateFields = {};
+    if (username) updateFields.username = username;
+    if (role) updateFields.role = role;
+    if (password) {
+        const salt = await bcrypt.genSalt(10);
+        updateFields.password = await bcrypt.hash(password, salt);
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(id, updateFields, { new: true });
+        if (!user) {
+            return NextResponse.json({ message: "Utente non trovato" }, { status: 404 });
+        }
+        return NextResponse.json({ message: "Utente modificato" }, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Errore durante l'aggiornamento dell'utente" }, { status: 500 });
+    }
 }
