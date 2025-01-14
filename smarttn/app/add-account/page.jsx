92d00e005f7c284188eee614e7a2f255cc46ee0f
RoccoIntini <rocco.intini@studenti.unitn.it>
2025-01-14 11:59:33 +0100

    Update page.jsx

--- smarttn/app/add-account/page.jsx
+++ smarttn/app/add-account/page.jsx
@@ -19,7 +19,7 @@ export default function AddUser() {
         }
 
         try {
-            const res = await fetch('http://localhost:3000/api/users', {
+            const res = await fetch('https://ingegneria-del-software-phcc.onrender.com/api/users', {
                 method: 'POST',
                 headers: {
                     "Content-type": "application/json"
