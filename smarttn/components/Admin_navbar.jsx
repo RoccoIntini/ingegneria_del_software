3817dc39a45618ad3a1a0f3456eff459f200f039
Filippo Tancon <filippotancon2004@gmail.com>
2025-01-15 16:53:17 +0100

    logout admin

--- smarttn/components/Admin_navbar.jsx
+++ smarttn/components/Admin_navbar.jsx
@@ -30,37 +30,37 @@ export default function Admin_mavbar() {
   };
 
   return (
-    <nav className="text-2xl flex items-center bg-red-800 justify-between py-4 text-white my-6 rounded-lg">
-      <div className="pl-20 font-bold">Benvenuto {nomeutente || 'utente'}</div>
-      <Link href={'/admin'} className="">Home admin</Link>
-      
-      {/* Dropdown Corsi */}
-      <div className="relative group">
-        <button className="py-2">Corsi</button>
-        <div className="absolute left-0 hidden bg-white text-red-600 text-base shadow-lg w-40 group-hover:block">
-          <Link href="/admin/notAvailable" className="block px-4 py-2">Aggiungi corso</Link>
-          <Link href="/admin/viewCourse" className="block px-4 py-2">Lista corsi</Link>
-          <Link href="/admin/notAvailable" className="block px-4 py-2">Gestisci richieste</Link>
-        </div>
-      </div>
+    <nav className="text-2xl flex items-center bg-red-800 justify-around py-4 text-white my-6 rounded-lg">
+  <div className="font-bold">Benvenuto {nomeutente || 'utente'}</div>
+  <Link href={'/admin'}>Home admin</Link>
 
-      {/* Dropdown Account */}
-      <div className="relative group">
-        <button className="pr-20">Account</button>
-        <div className="absolute left-0 hidden bg-white text-red-600 text-base shadow-lg w-40 group-hover:block">
-            <Link href="/admin/view-user" className="block px-4 py-2">Lista account</Link>
-            <Link href="/admin/add-account" className="block px-4 py-2">Aggiungi account</Link>
-        </div>
-      </div>
+  {/* Dropdown Corsi */}
+  <div className="relative group">
+    <button className="py-2">Corsi</button>
+    <div className="absolute left-0 hidden bg-white text-red-600 text-base shadow-lg w-40 group-hover:block">
+      <Link href="/admin/notAvailable" className="block px-4 py-2">Aggiungi corso</Link>
+      <Link href="/admin/viewCourse" className="block px-4 py-2">Lista corsi</Link>
+      <Link href="/admin/notAvailable" className="block px-4 py-2">Gestisci richieste</Link>
+    </div>
+  </div>
 
-      {/* Dropdown Profilo */}
-      <div className="relative group">
-            <button className="pr-20">Profilo</button>
-            <div className="absolute left-0 hidden bg-white text-red-600 text-base shadow-lg w-40 group-hover:block">
-            <button onClick={handleLogout} className="block px-4 py-2">Logout</button>
-            </div>
-          </div>
+  {/* Dropdown Account */}
+  <div className="relative group">
+    <button>Account</button>
+    <div className="absolute left-0 hidden bg-white text-red-600 text-base shadow-lg w-40 group-hover:block">
+      <Link href="/admin/view-user" className="block px-4 py-2">Lista account</Link>
+      <Link href="/admin/add-account" className="block px-4 py-2">Aggiungi account</Link>
+    </div>
+  </div>
 
-    </nav>
+  {/* Dropdown Profilo */}
+  <div className="relative group">
+    <button>Profilo</button>
+    <div className="absolute left-0 hidden bg-white text-red-600 text-base shadow-lg w-40 group-hover:block">
+      <button onClick={handleLogout} className="block px-4 py-2">Logout</button>
+    </div>
+  </div>
+</nav>
+
   );
 }
