900fe30eb70e2764151746100b4dd8586bccb391
Filippo Tancon <filippotancon2004@gmail.com>
2025-01-15 11:49:19 +0100

    2

--- smarttn/app/api/users/login/route.js
+++ smarttn/app/api/users/login/route.js
@@ -5,8 +5,10 @@ import { parse } from 'cookie';
 import jwt from 'jsonwebtoken';
 import { NextResponse } from "next/server";
 
+const SECRET_KEY = "Abgdkgdh";
+
 export async function POST(request) {
-    if(req.method === 'POST'){
+    
         const { username, password } = await request.json();
         await connectMongoDB();
 
@@ -22,8 +24,8 @@ export async function POST(request) {
 
         const payload={ userid: user._id, username: user.username, role: user.role};
 
-        console.log("SCIPOLLA");
-        const token = jwt.sign(payload, "SCIPOLLA", {expiresIn: "1h"});
+        console.log("Abgdkgdh");
+        const token = jwt.sign(payload, "Abgdkgdh", {expiresIn: "1h"});
 
         const response = NextResponse.json({ message: "Accesso effettuato", token, userId: user._id, role: user.role }, { status: 200 });
         response.cookies.set('tokenjwt', token, {
@@ -32,29 +34,26 @@ export async function POST(request) {
             path: '/',
         });
         return response;
-    }
     
 }
 
-export default function handler(req, res) {
-    if(req.method === 'GET'){
-        const cookies = req.headers.cookie; // Ottieni i cookie dalla richiesta
-        if (!cookies) {
-        res.status(401).json({ message: 'Nessun cookie trovato' });
-        return;
-        }
-    
-        const parsedCookies = parse(cookies); // Analizza i cookie
-        const token = parsedCookies.tokenjwt; // Leggi il cookie specifico
-        console.log("Token lato server:", token);
-    
-        if (!token) {
-        res.status(401).json({ message: 'Token non trovato' });
-        return;
-        }
-    
-        res.status(200).json({ message: 'Token trovato', token });
+export async function GET(request) {
+    const cookies = request.headers.get("cookie"); // Ottieni i cookie dall'intestazione
+    if (!cookies) {
+      return NextResponse.json({ message: "Nessun cookie trovato" }, { status: 401 });
     }
-    
+  
+    const parsedCookies = parse(cookies); // Analizza i cookie
+    const token = parsedCookies.tokenjwt; // Leggi il cookie specifico
+    if (!token) {
+      return NextResponse.json({ message: "Token non trovato" }, { status: 401 });
+    }
+  
+    try {
+      const decoded = jwt.verify(token, SECRET_KEY); // Verifica il token
+      return NextResponse.json({ message: "Token valido", decoded });
+    } catch (error) {
+      return NextResponse.json({ message: "Token non valido", error: error.message }, { status: 401 });
+    }
   }
   
