f2147915f5b89f294a57341976f154c3f2b0514e
RoccoIntini <rocco.intini@studenti.unitn.it>
2025-01-15 16:05:45 +0100

    Update page.jsx

--- smarttn/app/notAvailable/page.jsx
+++ smarttn/app/notAvailable/page.jsx
@@ -1,10 +1,17 @@
-
 export default function notAvailable() {
-  
-    return <div>
-        <h1>
-            QUESTA FUNZIONE DEL SITO NON E'ANCORA DISPONIBILE
-        </h1>
-    </div>
- 
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+                textAlign: "center",
+            }}
+        >
+            <h1 style={{ fontSize: "2rem" }}>
+                QUESTA FUNZIONE DEL SITO NON E' ANCORA DISPONIBILE
+            </h1>
+        </div>
+    );
 }
