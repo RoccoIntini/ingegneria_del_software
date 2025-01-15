00d0fbf679160aae175d710e2a2cb5a8261d2a8d
RoccoIntini <rocco.intini@studenti.unitn.it>
2025-01-15 16:28:59 +0100

    Update page.jsx

--- smarttn/app/page.jsx
+++ smarttn/app/page.jsx
@@ -1,10 +1,19 @@
-
 export default function Home() {
-  
-    return <div>
-        <h1>
-            BENVENUTO SU SmartTn
-        </h1>
-    </div>
- 
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "flex-start",
+                padding: "10vh 5vw",
+                height: "30vh",
+                textAlign: "center",
+                color: "#b30000",
+            }}
+        >
+            <h1 style={{ fontSize: "2rem" }}>
+                BENVENUTO SU SmartTn
+            </h1>
+        </div>
+    );
 }
