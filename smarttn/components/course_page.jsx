1ec89ba2a4f883c30648ed165d9b7d558b16e3ae
Filippo Tancon <filippotancon2004@gmail.com>
2025-01-15 18:44:33 +0100

    pagina corso1

--- smarttn/components/course_page.jsx
+++ smarttn/components/course_page.jsx
@@ -5,11 +5,11 @@ export default function coursePage(){
 
     return (
         <div className="course-card">
-          <div className="header">
+          <div className="course-header">
             <h1>TITOLO</h1>
             <p>descrizione corso</p>
           </div>
-          <div className="content">
+          <div className="course-content">
             <div className="course-text">scemo</div>
           </div>
         </div>
