6fc41b6241e2105891550161eff8075d6943e588
Filippo Tancon <filippotancon2004@gmail.com>
2025-01-15 14:07:05 +0100

    nome utente

--- smarttn/components/Admin_navbar.jsx
+++ smarttn/components/Admin_navbar.jsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Admin_mavbar() {
   const [nomeutente, setNomeutente] = useState('');
