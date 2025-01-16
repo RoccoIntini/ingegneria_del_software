390a1263f307a4e9288866546362a9e7913c0c2a
RoccoIntini <rocco.intini@studenti.unitn.it>
2025-01-16 11:46:34 +0100

    Update page.jsx

--- smarttn/app/admin/add-course/page.jsx
+++ smarttn/app/admin/add-course/page.jsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import "../add-course.css";
 
 export default function AddCourse() {
   const [title, setTitle] = useState("");
